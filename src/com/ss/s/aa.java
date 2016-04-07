24ef13d648a5c07515a4719de787e0f8e512b71c
Sealy <sealy.dong@gmail.com>
2016-04-07 16:18:21 +0800

    xx.1

--- src/com/ss/s/aa.java
+++ src/com/ss/s/aa.java
@@ -1,12 +1,15 @@
 package com.ss.s;
 
 public class aa {
+public static void main(String[] args) {
+	
 
-private void mian() {
 	// TODO Auto-generated method stub
 	System.out.println("sjn");
 	System.out.println("mmmmmmmmmmm");
 	System.out.println("kkkkkkkkk");
+	System.out.println("dddddd");
+	System.out.println("''''");
 }
 }
  
