90e3f8ef345480e1aa2bf649cb67835c7c977249
Sealy <sealy.dong@gmail.com>
2016-04-08 11:10:04 +0800

    test....

--- src/com/ss/s/aa.java
+++ src/com/ss/s/aa.java
@@ -10,6 +10,9 @@ public static void main(String[] args) {
 	System.out.println("kkkkkkkkk");
 	System.out.println("dddddd");
 	System.out.println("''''");
+	for (int i = 0; i < args.length; i++) {
+		System.out.println("test-----------");
+	}
 }
 }
  
